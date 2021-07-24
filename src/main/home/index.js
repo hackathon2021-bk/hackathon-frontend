26e09ba39221e0d7ea47b8e807fe1f048f3eb092
sky1307 <vietpham1307.sky@gmail.com>
2021-07-24 09:41:01 +0700

    vp3

--- src/main/home/index.js
+++ src/main/home/index.js
@@ -1,32 +1,20 @@
-<<<<<<< HEAD
 import {Col ,Row, Card, Layout} from "antd";
 import Videohome from './videohome';
 function HomePage() {
-=======
-import {Col ,Row} from "antd";
-
-export default function HomePage() {
-  
->>>>>>> a35b3cb79ab8f634f6d59d068e4f64a961d73407
   return <>
-  <body>
-    
+  
+    <body>
         <section id="mohinh" >
             <Videohome/>
-        </section>
-
-          <section id="loiich"  >
-              <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-              <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        </section> 
+              
+        <section id="loiich">     
               <Layout>
                 <img src="/loiich.png"  width="100%" left="0" ></img>
-              </Layout>
-              
-              
-          </section>
-     
-        
-
-  </body>
+              </Layout>     
+        </section>
+    </body> 
+  
   </>
 }
+export default HomePage;
